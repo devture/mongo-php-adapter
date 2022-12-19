4586d500e7910d2bb2a1bb9cc48b026c4e2f9657
Slavi Pantaleev <slavi@devture.com>
2022-12-19 12:48:47 +0200

    Improve PHP 8.2 compatibility - fix `Use of "self" in callables is deprecated` error
    Fixes:
    
    > Deprecated: Use of "self" in callables is deprecated in /../vendor/alcaeus/mongo-php-adapter/lib/Alcaeus/MongoDbAdapter/TypeConverter.php on line 180

--- lib/Alcaeus/MongoDbAdapter/TypeConverter.php
+++ lib/Alcaeus/MongoDbAdapter/TypeConverter.php
@@ -178,7 +178,7 @@ class TypeConverter
             case $value instanceof Model\BSONDocument:
             case $value instanceof Model\BSONArray:
                 return array_map(
-                    ['self', 'toLegacy'],
+                    [__CLASS__, 'toLegacy'],
                     $value->getArrayCopy()
                 );
             default:
